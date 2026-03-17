a0d57e9f640552de0975c4d95e308247ca40c097
automatizadoconia <vallverdu04@gmail.com>
2026-03-17 13:20:08 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://automatizadoconia.github.io',
+  base: '/',
   output: 'static',
 
   vite: {
